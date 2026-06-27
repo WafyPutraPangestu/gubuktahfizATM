7eab1934c3c74cf7a5d04111285d9f9ff6b1de0e
WafyPutraPangestu <wafy@raharja.info>
2026-06-27 17:35:12 +0700

    ubah seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name'     => 'Admin Ustadz',
             'email'    => 'admin@gmail.com',
             'password' => Hash::make('password'),
